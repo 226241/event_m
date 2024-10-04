e8548248a4195a0ba8b074dfa887117410993a98
haridas roy <haricse20hstu@gmail.com>
2024-10-04 06:02:02 -0700

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'views/onboarding_screen/onboarding_screen.dart';
 
 void main() async{
+  Firebase.initializeApp(options: FirebaseOptions(apiKey: apiKey, appId: appId, messagingSenderId: messagingSenderId, projectId: projectId));
   runApp(const MyApp());
 }
-////////////
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
