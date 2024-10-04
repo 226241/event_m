9398924222d470c0dcd193760808ac38ee2b34d3
ShRudra88 <salhawathosenrudra503@gmail.com>
2024-10-05 01:38:53 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'views/onboarding_screen/onboarding_screen.dart';
 
+// rudra
 void main() async{
   //Firebase.initializeApp(options: FirebaseOptions(apiKey: apiKey, appId: appId, messagingSenderId: messagingSenderId, projectId: projectId));
   runApp(const MyApp());
